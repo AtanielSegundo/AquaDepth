891fc5161ee745de89ae2048ce911327bb1498cf
10ataniel <10ataniel@gmail.com>
2023-12-01 18:50:33 -0300

    minor changes

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
 from flask import Flask
+from typing import Literal,List,Union
 
-api = Flask("AquaDepth Api")
+class ApiInterface:
+    def __init__(self,mode:Literal["server","dev"]="server") -> None:
+        if mode == "server":
+            self.api = Flask("AquaDepth Api")
+        elif "dev":
+            pass
+    @Flask.route("/<name>")
+    def hello_world(name:str,*args):
+        return f"<b>hello {name}<\b> and {args}"
 
-@api.route("/<name>")
-def hello_world(name:str,*args):
-    return f"<b>hello {name}<\b> and {args}"
+if __name__ == "__main__":
+    import argparse
+    from utils.generics import verify_path,config_log
+    parser = argparse.ArgumentParser(
+             description='Aqua Depth Arguments',
+             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--mode",metavar="Mode",nargs="?",
+                        default="server")
+    parser.add_argument("--test",action="store_true",default=False)
+    parser.add_argument("--logpath",metavar="Path",
+                        type=str,default="logs/aqua.log")
+    args = parser.parse_args()
+
+    log_path = args.logpath 
+    if verify_path(log_path):
+        logger = config_log(save_path=log_path)
+    else:
+        from os.path import dirname
+        from os import mkdir
+        if verify_path(dir_name:=dirname(log_path)):
+            logger = config_log(save_path=log_path)
+        else:
+            mkdir(dir_name)
+            logger = config_log(save_path=log_path)
+    
+    
+    
---
+++ utils/generics.py
@@ -0,0 +1,37 @@
+from typing import Literal
+
+def config_log(save_path:str,with_time=True):
+    import logging
+    from os.path import exists,basename
+    from os import remove
+
+    if exists(save_path):
+        remove(save_path)
+
+    if with_time:
+        formatter = logging.Formatter(
+            "[%(levelname)s] [%(asctime)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    else:
+        formatter = logging.Formatter("%(message)s")
+    
+    file_handler = logging.FileHandler(save_path)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(basename(save_path).split('.')[0])
+    logger.setLevel(level=logging.INFO)
+    logger.handlers = [file_handler, console_handler]
+
+def verify_path(path:str,mode:Literal["dir","file","url"]) -> bool:
+    if mode == "dir":
+        from os.path import isdir
+        return True if isdir(path) else False
+        
+    elif mode == "file":
+        from os.path import exists
+        return True if exists(path) else False
+    
+    elif mode == "url":
+        ...
